3733ea767b5671674d7c92e730f45f6074ffdd0a
yusrilihzaM <mohyusrilihzamaulana@gmail.com>
2021-04-22 21:08:04 +0700

    tentang

--- app/src/main/java/com/yusril/kpperamalan/activity/tentang/TentangViewPager.kt
+++ app/src/main/java/com/yusril/kpperamalan/activity/tentang/TentangViewPager.kt
@@ -3,14 +3,10 @@ package com.yusril.kpperamalan.activity.tentang
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.yusril.kpperamalan.activity.ses.SesChartFragment
-import com.yusril.kpperamalan.activity.ses.SesEvaluasiFragment
-import com.yusril.kpperamalan.activity.ses.SesHitungFragment
-import com.yusril.kpperamalan.activity.ses.SesRamalFragment
 
 class TentangViewPager(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
-        return 4
+        return 1
     }
 
     override fun createFragment(position: Int): Fragment {
